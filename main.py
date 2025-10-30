be5ada9aa42173030233f728ca2bde62fc7704a7
Aibek Urazov(Macbook) <ibek-94@mail.ru>
2025-10-31 03:37:45 +0500

    Добавлен метод POST

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
 import uvicorn
 
 app = FastAPI()
@@ -10,16 +11,31 @@ books = [
 ]
 
 
-@app.get("/books", tags=["Книги"], summary="Книги в вродаже")
+@app.get("/books", tags=["Книги"], summary="Получить все книги")
 def all_books():
     return books
 
-@app.get("/books/{book_id}", tags=["Книги"], summary="Книги в вродаже")
+@app.get("/books/{book_id}", tags=["Книги"], summary="Получить конкретную книгу")
 def get_book(book_id: int):
     for book in books:
         if book["id"] == book_id:
             return book
     raise HTTPException(status_code=404, detail="Книга не найдена")
+
+class newBook(BaseModel):
+    name: str
+    Author: str
+
+@app.post("/books", tags=["Добавление книги"], summary="Добавление книги")
+def create_book(new_book: newBook):
+    books.append(
+        {   "id": len(books) + 1, 
+            "name": new_book.name,
+            "Author": new_book.Author
+            }
+    )
+
+
 
 
 if __name__ == "__main__":
