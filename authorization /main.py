6c5bf4ea959b2fc9cdf20dcd25c66731c35be134
Aibek Urazov(Macbook) <ibek-94@mail.ru>
2025-11-05 08:08:42 +0500

    add get

--- authorization /main.py
+++ authorization /main.py
@@ -1,12 +1,24 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from data import students
 import uvicorn
 
 app = FastAPI()
 
+@app.get("/")
+def start_page():
+    return {"status": "ok"}
+
 @app.get("/api/v1/students", tags=["Student list"])
 def get_students():
     return students 
+
+@app.get("/api/v1/students/{student_id}")
+def get_student(student_id: int):
+    for student in students:
+        if student["student_id"] == student_id:
+            return student
+    raise HTTPException(status_code=404, detail="Студент с таким id не найдет")
+        
 
 
 if __name__ == "__main__":
